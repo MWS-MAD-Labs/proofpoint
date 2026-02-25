0909519eaed1c2a34e7dfe349d8f6437da1fcc1e
faisalnh <faisal@millennia21.id>
2026-02-25 12:11:23 +0700

    chore: Remove obsolete database migrations, scripts, and documentation.

--- database/migrations/004_workflow_naming.sql
+++
@@ -1,8 +0,0 @@
--- Migration: Workflow Naming Support
--- Add a name column to department_roles to allow custom workflow naming
-
-BEGIN;
-
-ALTER TABLE public.department_roles ADD COLUMN name TEXT;
-
-COMMIT;
